728f3f56a2069a5e35d3b5a0f90841ae36cefa1d
Pascal Berger <pascal.berger@gmail.com>
2017-02-10 13:47:54 +0100

    Initial version for Cake.Yeoman addin

---
+++ src/Cake.Yeoman.Tests/YeomanRunnerTests.cs
@@ -0,0 +1,103 @@
+﻿namespace Cake.Yeoman.Tests
+{
+    using Shouldly;
+    using Xunit;
+
+    public sealed class YeomanRunnerTests
+    {
+        public sealed class TheRunGeneratorMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Generator_Name_Is_Null()
+            {
+                // Given
+                var fixture = new YeomanRunnerFixture
+                {
+                    GeneratorName = null
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                result.IsArgumentNullException("generator");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Generator_Name_Is_Empty()
+            {
+                // Given
+                var fixture = new YeomanRunnerFixture
+                {
+                    GeneratorName = string.Empty
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                result.IsArgumentOutOfRangeException("generator");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Generator_Name_Is_Whitespace()
+            {
+                // Given
+                var fixture = new YeomanRunnerFixture()
+                {
+                    GeneratorName = "   "
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                result.IsArgumentOutOfRangeException("generator");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Settings_Are_Null()
+            {
+                // Given
+                var fixture = new YeomanRunnerFixture
+                {
+                    GeneratorName = "Foo",
+                    Settings = null
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                result.IsArgumentNullException("settings");
+            }
+
+            [Fact]
+            public void No_Settings_Specified_Should_Execute_Command_Without_Arguments()
+            {
+                var fixture = new YeomanRunnerFixture
+                {
+                    GeneratorName = "Foo"
+                };
+
+                var result = fixture.Run();
+
+                result.Args.ShouldBe(fixture.GeneratorName);
+            }
+
+            [Fact]
+            public void Arguments_Should_Be_Added_If_Settings_Are_Passed()
+            {
+                var fixture = new YeomanRunnerFixture()
+                {
+                    GeneratorName = "Foo"
+                };
+                fixture.Settings.Arguments.Add("Bar");
+
+                var result = fixture.Run();
+
+                result.Args.ShouldBe("Foo Bar");
+            }
+        }
+    }
+}
---
+++ src/Cake.Yeoman/YeomanRunnerSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace Cake.Yeoman
+{
+    using System.Collections.Generic;
+    using Core;
+    using Core.IO;
+    using Core.Tooling;
+
+    /// <summary>
+    /// Settings for <see cref="YeomanRunner"/> .
+    /// </summary>
+    public class YeomanRunnerSettings : ToolSettings
+    {
+        private readonly List<string> arguments = new List<string>();
+
+        /// <summary>
+        /// Gets arguments to pass to the target script.
+        /// </summary>
+        public IList<string> Arguments => this.arguments;
+
+        /// <summary>
+        /// Evaluates the settings and writes them into <paramref name="args"/>.
+        /// </summary>
+        /// <param name="args">Argument builder to which the settings should be added.</param>
+        internal void Evaluate(ProcessArgumentBuilder args)
+        {
+            foreach (var arg in this.Arguments)
+            {
+                args.Append(arg);
+            }
+        }
+    }
+}
